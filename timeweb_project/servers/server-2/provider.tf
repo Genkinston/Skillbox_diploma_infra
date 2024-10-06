d5a49b10a00b890429977b45e6183a6e96120517
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-10-06 14:04:59 +0700

    reformat catalogs

---
+++ timeweb_project/servers/server-2/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    twc = {
+      source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
+    }
+  }
+  required_version = ">= 1.4.4"
+}
+
+provider "twc" {
+  token = "${var.TWC_KEY}"
+}
+
+
