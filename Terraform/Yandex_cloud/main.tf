212b7241c56ffaa442379d094a51741f022298b3
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-11-10 18:33:42 +0700

    Update main.tf

--- Terraform/Yandex_cloud/main.tf
+++ Terraform/Yandex_cloud/main.tf
@@ -12,16 +12,3 @@ provider "yandex" {
   service_account_key_file = "/home/lurk/.config/yandex-cloud/authorized_key.json"
   folder_id = "b1ghj3gm3a5ud4i8h84n"
 }
-
-resource "yandex_vpc_network" "default" {
-  name = "my-network"
-  folder_id = "b1ghj3gm3a5ud4i8h84n"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name = "subnet1"
-  zone = "ru-central1-a"
-  folder_id = "b1ghj3gm3a5ud4i8h84n"
-  network_id = "${yandex_vpc_network.default.id}"
-  v4_cidr_blocks = ["192.168.1.0/24"]
-}
