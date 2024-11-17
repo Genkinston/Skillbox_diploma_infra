3ff61d27f84665294e759e23b5ca3af5fbd2801a
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-11-17 20:03:22 +0700

    Update bucket_for_domain.tf

--- Terraform/Yandex_cloud/bucket_for_domain.tf
+++ Terraform/Yandex_cloud/bucket_for_domain.tf
@@ -1,6 +1,10 @@
 resource "yandex_storage_bucket" "domain_bucket" {
     bucket = "genkinstonlurk.ru"
+    bucket_domain_name = "genkinstonlurk.ru"
     acl    = "public-read"
 
+    website {
+      index_document = "balancernginx.genkinstonlurk.ru/index.html"
+    }
 }
 
