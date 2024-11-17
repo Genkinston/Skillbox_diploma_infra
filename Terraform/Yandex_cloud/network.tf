4e91c12bc8df7902810d95b1d44b7b1636629679
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-11-17 20:03:28 +0700

    Update network.tf

--- Terraform/Yandex_cloud/network.tf
+++ Terraform/Yandex_cloud/network.tf
@@ -10,3 +10,38 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id = "${yandex_vpc_network.default.id}"
   v4_cidr_blocks = ["192.168.1.0/24"]
 }
+
+# Создание группы безопасности
+
+resource "yandex_vpc_security_group" "web_server_sg1" {
+  name        = "Security_group_1"
+  network_id  = "${yandex_vpc_network.default.id}"
+  ingress {
+    protocol       = "TCP"
+    description    = "http"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "https"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "any"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
