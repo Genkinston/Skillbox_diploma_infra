4656d1a46b9af450ae343e3bd5ac340795afe8bd
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-11-17 17:52:17 +0700

    Update network.tf

--- Terraform/Yandex_cloud/network.tf
+++ Terraform/Yandex_cloud/network.tf
@@ -10,32 +10,3 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id = "${yandex_vpc_network.default.id}"
   v4_cidr_blocks = ["192.168.1.0/24"]
 }
-
-resource "yandex_dns_zone" "zone1" {
-  name        = "domain-bucket-zone-1"
-  description = "desc"
-
-  labels = {
-    label1 = "label-1-value"
-  }
-
-  zone             = "genkinstonlurk.ru."
-  public           = true
-  private_networks = [yandex_vpc_network.default.id]
-}
-
-resource "yandex_dns_recordset" "server" {
-  zone_id = yandex_dns_zone.zone1.id
-  name    = "server1"
-  type    = "A"
-  ttl     = 200
-  data    = ["192.168.1.12"]
-}
-
-resource "yandex_dns_recordset" "balancer-nginx" {
-  zone_id = yandex_dns_zone.zone1.id
-  name    = "balancernginx"
-  type    = "A"
-  ttl     = 200
-  data    = ["192.168.1.11"]
-}
