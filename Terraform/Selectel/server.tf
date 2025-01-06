77d74993bfd5e5f9011c0f38cd004a1fb0c63ec9
Genkinston <69857989+Genkinston@users.noreply.github.com>
2025-01-06 15:25:59 +0700

    Update server.tf

--- Terraform/Selectel/server.tf
+++ Terraform/Selectel/server.tf
@@ -1,24 +1,9 @@
-#Создать флейвор с сетевым диском
-resource "openstack_compute_flavor_v2" "flavor_1" {
-  name      = "custom-flavor-with-network-volume"
-  vcpus     = 2
-  ram       = 2048
-  disk      = 0
-  is_public = false
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 #Получение образа
 
 data "openstack_images_image_v2" "image_1" {
   name        = "Ubuntu 24.04 LTS 64-bit"
   most_recent = true
   visibility  = "public"
-  container_format = "bare"
-  disk_format      = "qcow2"
 
   depends_on = [
     selectel_vpc_project_v2.project_genkinstonlurk,
@@ -32,7 +17,7 @@ resource "openstack_blockstorage_volume_v3" "volume_1" {
   name                 = "boot-volume-for-server"
   size                 = "5"
   image_id             = data.openstack_images_image_v2.image_1.id
-  volume_type          = "fast.ru-9a"
+  volume_type          = "basic.ru-9a"
   availability_zone    = "ru-9a"
   enable_online_resize = true
 
@@ -45,12 +30,14 @@ resource "openstack_blockstorage_volume_v3" "volume_1" {
 
 resource "openstack_compute_instance_v2" "server_1" {
   name              = "server"
-  flavor_id         = openstack_compute_flavor_v2.flavor_1.id
+  flavor_id         = "1"
   key_pair          = selectel_vpc_keypair_v2.keypair_1.name
   availability_zone = "ru-9a"
 
   network {
     port = openstack_networking_port_v2.port_1.id
+    #name = openstack_networking_network_v2.network_1.name
+    #fixed_ip_v4 = "192.168.200.4"
   }
 
   lifecycle {
