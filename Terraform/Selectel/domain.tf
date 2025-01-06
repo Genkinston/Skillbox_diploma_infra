907b2abe196e6ec10ba40e10941716c2703cd982
Genkinston <69857989+Genkinston@users.noreply.github.com>
2025-01-06 15:52:15 +0700

    Update domain.tf

--- Terraform/Selectel/domain.tf
+++ Terraform/Selectel/domain.tf
@@ -1,7 +1,7 @@
 #Описание доменной зоны
 
 resource "selectel_domains_zone_v2" "zone_1" {
-  name       = "genkinstonlurk.ru."
+  name       = "genkinstonlurk.tech."
   project_id = selectel_vpc_project_v2.project_genkinstonlurk.id
 }
 
