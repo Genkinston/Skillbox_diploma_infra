17181a13be97720be5fecc6d4c5d5052aacb3d60
Genkinston <69857989+Genkinston@users.noreply.github.com>
2025-01-08 00:10:51 +0700

    Update load_balancer.tf

--- Terraform/Selectel/load_balancer.tf
+++ Terraform/Selectel/load_balancer.tf
@@ -10,18 +10,35 @@ resource "openstack_lb_loadbalancer_v2" "load_balancer_1" {
 
 resource "openstack_lb_listener_v2" "listener_1" {
   name            = "listener"
-  protocol        = "TCP"
+  protocol        = "HTTP"
   protocol_port   = "80"
   loadbalancer_id = openstack_lb_loadbalancer_v2.load_balancer_1.id
+}
+
+#Создать правило⁠ http
+
+resource "openstack_lb_listener_v2" "listener_2" {
+  name            = "listener_ssh"
+  protocol        = "TCP"
+  protocol_port   = "22"
+  loadbalancer_id = openstack_lb_loadbalancer_v2.load_balancer_1.id
+  admin_state_up = true
 }
 
 #Создать целевую группу⁠
 
 resource "openstack_lb_pool_v2" "pool_1" {
   name        = "pool"
-  protocol    = "PROXY"
+  protocol    = "HTTP"
   lb_method   = "ROUND_ROBIN"
   listener_id = openstack_lb_listener_v2.listener_1.id
+}
+
+resource "openstack_lb_pool_v2" "pool_ssh" {
+  name        = "pool_ssh"
+  protocol    = "TCP"
+  lb_method   = "ROUND_ROBIN"
+  listener_id = openstack_lb_listener_v2.listener_2.id
 }
 
 #Добавить сервер в целевую группу http
@@ -31,7 +48,15 @@ resource "openstack_lb_member_v2" "member_1" {
   subnet_id     = openstack_networking_subnet_v2.subnet_1.id
   pool_id       = openstack_lb_pool_v2.pool_1.id
   address       = openstack_compute_instance_v2.server_1.network[0].fixed_ip_v4
-  protocol_port = "80"
+  protocol_port = "8080"
+}
+
+resource "openstack_lb_member_v2" "member_ssh" {
+  name          = "member_ssh"
+  subnet_id     = openstack_networking_subnet_v2.subnet_1.id
+  pool_id       = openstack_lb_pool_v2.pool_ssh.id
+  address       = openstack_compute_instance_v2.server_1.network[0].fixed_ip_v4
+  protocol_port = "22"
 }
 
 #Создать проверку доступности⁠ http
@@ -43,24 +68,5 @@ resource "openstack_lb_monitor_v2" "monitor_1" {
   delay       = "10"
   timeout     = "4"
   max_retries = "5"
-}
-
-#Создать правило⁠ ssh
-
-resource "openstack_lb_listener_v2" "listener_ssh" {
-  name            = "ssh_listener"
-  protocol        = "TCP"
-  protocol_port   = "22"
-  loadbalancer_id = openstack_lb_loadbalancer_v2.load_balancer_1.id
-}
-
-#Добавить сервер в целевую группу ssh
-
-resource "openstack_lb_member_v2" "member_ssh" {
-  name          = "ssh_member"
-  subnet_id     = openstack_networking_subnet_v2.subnet_1.id
-  pool_id       = openstack_lb_pool_v2.pool_1.id
-  address       = openstack_compute_instance_v2.server_1.network[0].fixed_ip_v4
-  protocol_port = "22"
 }
 
